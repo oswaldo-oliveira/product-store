3dd9ca7ef93de6838fa36a6bd206fdf4205110e7
Oswaldo Oliveira <oswaldo.dev.oliveira@gmail.com>
2024-05-27 19:30:14 -0300

    :sparkles: feat: create user and configs

--- cmd/productstore/main.go
+++ cmd/productstore/main.go
@@ -1,3 +1,12 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+
+	"github.com/oswaldo-oliveira/productstore/configs"
+)
+
+func main() {
+	conf, _ := configs.LoadConfig(".")
+	fmt.Printf(conf.DBDriver)
+}
