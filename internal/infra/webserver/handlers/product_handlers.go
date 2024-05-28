1ebcc7ecae0af48cf0049e483c000b665c457e14
Oswaldo Oliveira <oswaldo.dev.oliveira@gmail.com>
2024-05-28 18:00:16 -0300

    :sparkles: feat: create products handlers.

---
+++ internal/infra/webserver/handlers/product_handlers.go
@@ -0,0 +1,41 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/oswaldo-oliveira/productstore/internal/dto"
+	"github.com/oswaldo-oliveira/productstore/internal/entities"
+	"github.com/oswaldo-oliveira/productstore/internal/infra/database"
+)
+
+type ProductHandler struct {
+	ProductDB database.ProductInterface
+}
+
+func NewProductHandler(db database.ProductInterface) *ProductHandler {
+	return &ProductHandler{ProductDB: db}
+}
+
+func (h *ProductHandler) CreateProduct(w http.ResponseWriter, r *http.Request) {
+	var product dto.ProductInput
+
+	err := json.NewDecoder(r.Body).Decode(&product)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	p, err := entities.NewProduct(product.Name, product.Price)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	err = h.ProductDB.Create(p)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
+}
