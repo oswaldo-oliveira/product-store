38eeefd59aa45f9b204226adeecfd718f6e9bb27
Oswaldo Oliveira <oswaldo.dev.oliveira@gmail.com>
2024-05-28 17:59:32 -0300

    :sparkles: feat: create database instance and handle products

--- cmd/productstore/main.go
+++ cmd/productstore/main.go
@@ -1,12 +1,31 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
 
 	"github.com/oswaldo-oliveira/productstore/configs"
+	"github.com/oswaldo-oliveira/productstore/internal/entities"
+	"github.com/oswaldo-oliveira/productstore/internal/infra/database"
+	"github.com/oswaldo-oliveira/productstore/internal/infra/webserver/handlers"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
 func main() {
-	conf, _ := configs.LoadConfig(".")
-	fmt.Printf(conf.DBDriver)
+	_, err := configs.LoadConfig(".")
+	if err != nil {
+		panic(err)
+	}
+
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	db.AutoMigrate(&entities.Product{}, &entities.User{})
+
+	productDB := database.NewProduct(db)
+	productHandler := handlers.NewProductHandler(productDB)
+
+	http.HandleFunc("/products", productHandler.CreateProduct)
+	http.ListenAndServe(":8000", nil)
 }
