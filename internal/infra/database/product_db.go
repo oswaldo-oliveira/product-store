6c7b2f5d1b315db535d826b4fa0ccad8ac741a9c
Oswaldo Oliveira <oswaldo.dev.oliveira@gmail.com>
2024-05-28 11:36:47 -0300

    :sparkles: feat: create product db

---
+++ internal/infra/database/product_db.go
@@ -0,0 +1,40 @@
+package database
+
+import (
+	"github.com/oswaldo-oliveira/productstore/internal/entities"
+	"gorm.io/gorm"
+)
+
+type Product struct {
+	DB *gorm.DB
+}
+
+func NewProduct(db *gorm.DB) *Product {
+	return &Product{DB: db}
+}
+
+func (p *Product) Create(product *entities.Product) error {
+	return p.DB.Create(product).Error
+}
+
+func (p *Product) FindByID(id string) (*entities.Product, error) {
+	var product entities.Product
+	err := p.DB.First(&product, "id = ?", id).Error
+	return &product, err
+}
+
+func (p *Product) Update(product *entities.Product) error {
+	_, err := p.FindByID(product.ID.String())
+	if err != nil {
+		return err
+	}
+	return p.DB.Save(product).Error
+}
+
+func (p *Product) Delete(product *entities.Product) error {
+	_, err := p.FindByID(product.ID.String())
+	if err != nil {
+		return err
+	}
+	return p.DB.Delete(product).Error
+}
