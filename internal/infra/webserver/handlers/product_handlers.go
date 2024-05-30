3b87bfd30d84e712f31a3ed146bd50b6d3e876af
Oswaldo Oliveira <oswaldo.dev.oliveira@gmail.com>
2024-05-30 09:53:42 -0300

    :sparkles: feat: create get product by id handler.

--- internal/infra/webserver/handlers/product_handlers.go
+++ internal/infra/webserver/handlers/product_handlers.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"net/http"
 
+	"github.com/go-chi/chi/v5"
 	"github.com/oswaldo-oliveira/productstore/internal/dto"
 	"github.com/oswaldo-oliveira/productstore/internal/entities"
 	"github.com/oswaldo-oliveira/productstore/internal/infra/database"
@@ -38,4 +39,21 @@ func (h *ProductHandler) CreateProduct(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	w.WriteHeader(http.StatusCreated)
+}
+
+func (h *ProductHandler) GetProduct(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	if id == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	product, err := h.ProductDB.FindByID(id)
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(product)
 }
