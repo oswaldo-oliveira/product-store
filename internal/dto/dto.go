94c47fed68f2a816e186546cdf6715655fe776c5
Oswaldo Oliveira <oswaldo.dev.oliveira@gmail.com>
2024-06-03 16:59:02 -0300

    :sparkles: feat: create user dto

--- internal/dto/dto.go
+++ internal/dto/dto.go
@@ -4,3 +4,9 @@ type ProductInput struct {
 	Name  string  `json:"name"`
 	Price float64 `json:"price"`
 }
+
+type UserInput struct {
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
