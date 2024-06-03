31d82b70c9d74b1292d1ffd9c5b223d4d76ac7bf
Oswaldo Oliveira <oswaldo.dev.oliveira@gmail.com>
2024-06-03 19:50:51 -0300

    :sparkles: feat: create login handle

--- internal/infra/webserver/handlers/user_handlers.go
+++ internal/infra/webserver/handlers/user_handlers.go
@@ -3,18 +3,26 @@ package handlers
 import (
 	"encoding/json"
 	"net/http"
+	"time"
 
+	"github.com/go-chi/jwtauth"
 	"github.com/oswaldo-oliveira/productstore/internal/dto"
 	"github.com/oswaldo-oliveira/productstore/internal/entities"
 	"github.com/oswaldo-oliveira/productstore/internal/infra/database"
 )
 
 type UserHandler struct {
-	UserDB database.UserInterface
+	UserDB      database.UserInterface
+	Jwt         *jwtauth.JWTAuth
+	JwtExpireIn int
 }
 
-func NewUserHandler(userDB database.UserInterface) *UserHandler {
-	return &UserHandler{UserDB: userDB}
+func NewUserHandler(userDB database.UserInterface, jwt *jwtauth.JWTAuth, jwtExpireIn int) *UserHandler {
+	return &UserHandler{
+		UserDB:      userDB,
+		Jwt:         jwt,
+		JwtExpireIn: jwtExpireIn,
+	}
 }
 
 func (h *UserHandler) CreateUser(w http.ResponseWriter, r *http.Request) {
@@ -37,4 +45,37 @@ func (h *UserHandler) CreateUser(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	w.WriteHeader(http.StatusCreated)
+}
+
+func (h *UserHandler) Login(w http.ResponseWriter, r *http.Request) {
+	var user dto.JwtInput
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	u, err := h.UserDB.FindByEmail(user.Email)
+	if err != nil {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+	if !u.ValidatePassword(user.Password) {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	_, token, _ := h.Jwt.Encode(map[string]interface{}{
+		"sub": u.ID.String(),
+		"exp": time.Now().Add(time.Second * time.Duration(h.JwtExpireIn)).Unix(),
+	})
+	accessToken :=
+		struct {
+			AccessToken string `json:"access_token"`
+		}{
+			AccessToken: token,
+		}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(accessToken)
 }
