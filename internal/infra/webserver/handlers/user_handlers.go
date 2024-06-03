caefa2d9f68cccb4be4efc4069b7fbf836629a09
Oswaldo Oliveira <oswaldo.dev.oliveira@gmail.com>
2024-06-03 16:58:44 -0300

    :sparkles: feat: create user handle

---
+++ internal/infra/webserver/handlers/user_handlers.go
@@ -0,0 +1,40 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/oswaldo-oliveira/productstore/internal/dto"
+	"github.com/oswaldo-oliveira/productstore/internal/entities"
+	"github.com/oswaldo-oliveira/productstore/internal/infra/database"
+)
+
+type UserHandler struct {
+	UserDB database.UserInterface
+}
+
+func NewUserHandler(userDB database.UserInterface) *UserHandler {
+	return &UserHandler{UserDB: userDB}
+}
+
+func (h *UserHandler) CreateUser(w http.ResponseWriter, r *http.Request) {
+	var user dto.UserInput
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	u, err := entities.NewUser(user.Name, user.Email, user.Password)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	err = h.UserDB.Create(u)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
+}
